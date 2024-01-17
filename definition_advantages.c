a5b5e1bc82867e0b8615f99ef9acf2e84a806694
prorege <yangsehyuk1117@gmail.com>
2024-01-17 18:38:36 +0900

    Create definition_advantages.c
    Neso Academy DS 3

---
+++ definition_advantages.c
@@ -0,0 +1,22 @@
+//https://www.youtube.com/watch?v=2JroZSycSXs&list=PLBlnK6fEyqRj9lld8sWIUNwlKfdUoPd1Y&index=3
+
+/*
+
+1) Definition of Data Structures.
+ds는 시간과 공간의 차원에서 효율적으로 데이터를 조직하는 방식이다.
+다시 말해 adt를 실행하기 위해 사용되는 게 ds다.
+stack ADT를 실행하기 위해 우리는 ds나 linked list를 사용해야 한다. 
+
+"ADT는 우리에게 무엇이 완료되어야 하는지 말해주고
+DS는 우리에게 어떻게 할 것인지를 말해준다."
+
+하지만 특정 ADT에 대해 어떤 ds를 사용해야 하는지 어떻게 알수있을까?
+
+2) Which Data Structure to choose.
+time complexity
+
+3) Advantages of Data Structures
+효율성이라고 할 때 실행의 속도와 메모리 차지 공간을 고려해야 한다.
+Reusability 한번 사용되면 다시 사용할 수 있다.
+Abstraction 
+*/
