350825ed1fffeafc34bd71101643df0d498bfaeb
prorege <yangsehyuk1117@gmail.com>
2024-01-17 18:16:24 +0900

    Create introduction.c
    NESO ACADEMY DS 1

---
+++ introduction.c
@@ -0,0 +1,47 @@
+// https://www.youtube.com/watch?v=xLetJpcjHS0&list=PLBlnK6fEyqRj9lld8sWIUNwlKfdUoPd1Y&index=1
+
+/*
+Data Structures: The Introduction to Data Structures
+
+1) What is Data?
+Dictionary definition
+quantities, characters, symbols
+c = a + b;
+a, b are data
+
+2) The difference between Data and Information.
+Data :          TEERSAJ SI EMAN YM
+위는 collection of characters이다.
+Information :   MY NAME IS JASPERRET
+데이터가 시스템에 의해 잘 정렬되면 구조와 의미를 가진다.
+의미있거나 가공된 데이터는 정보라고 부를 수 있다.
+이를 위해 DATA STRUCTURE를 배우는 것
+
+3) What is Data Structure?
+데이터 스트럭쳐는 효율적으로 데이터를 사용하기 위해 ORGANIZE하는 학문입니다.
+ARRAY가 그 예입니다. ARRAY도 DS의 일종입니다.
+많은 변수를 만들기보다 ARRAY하나를 만드는 게 더 효율적이기 때문입니다.
+문자열을 저장하는 것은 문자들의 배열을 저장하는 것과 같습니다. 이게 배열이 필요한 이유입니다.
+
+4) Real-life examples of Data Structures.
+EX)1
+STACK은 REDO, UNDO에 많이 사용됩니다. 
+GOOGLE DOCS에서 어떤 문장을 친다고 해봅시다.
+THE PRIMARY GOAL OF NESO ACADEMY IS TO GIVE QUALITY EDUCATION
+이때 CTRL + Z를 통해 EVENT를 되돌릴 수 있는데 이때 일정 단위로 지워진 STRING을
+다시 표현할 수 있습니다. 이때 STACK이 사용됩니다.
+
+EX)2
+BITMAP으로 이미지를 저장할 때 어떤 ds가 사용될까요?
+ARRAY입니다.
+
+EX)3
+SOCAIL NETWORK에서 FRIENDSHIP을 사용할 때도 DS가 필요합니다.
+JAMES-------------------MARK
+  |                       |
+  |                       |
+  |                       |
+LIA---------------------LUCY
+
+이런 EDGE가 있다고 할 때 GRAPH라는 DS가 필요합니다.
+*/
